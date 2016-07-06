63e9368901f5c1d85a9e318b283ae50414eb4a7d
Димитър Кацаров < >
2016-07-06 13:37:17 +0300

    Added radio button and working connection

--- GiveBlood/app/src/main/java/digimark/giveblood/RegHelpMeActivity.java
+++ GiveBlood/app/src/main/java/digimark/giveblood/RegHelpMeActivity.java
@@ -1,12 +1,16 @@
 package digimark.giveblood;
 
+import android.app.ProgressDialog;
 import android.content.ContentValues;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -21,66 +25,147 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 import javax.net.ssl.HttpsURLConnection;
 
 
 public class RegHelpMeActivity extends AppCompatActivity {
 
+    private RadioGroup group;
+    private RadioButton radioButton;
+
     private ContentValues cv = new ContentValues();
-    private TextView asd;
+    private EditText name;
+    private EditText date_of_birth;
+    private EditText phone_number;
+    private EditText email;
+    private String blood_type = "A+";
+    private EditText city;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reg_help_me_acticity);
-        asd = (TextView) findViewById(R.id.Results);
+        name = (EditText) findViewById(R.id.NameTxt);
+        date_of_birth = (EditText) findViewById(R.id.dateTxt);
+        phone_number = (EditText) findViewById(R.id.PhoneTxt);
+        email = (EditText) findViewById(R.id.EmailTxt);
+        city = (EditText) findViewById(R.id.CityTxt);
+
     }
-    public void PostReq(View view){
-        /*try {
-            URL url = new URL("http://192.168.88.167/GiveBlood/register_consumer.php");
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
-            urlConnection.setReadTimeout(10000);
-            urlConnection.setConnectTimeout(15000);
-            urlConnection.setRequestMethod("POST");
-            urlConnection.setDoInput(true);
-            urlConnection.setDoOutput(true);
+    public  void onRadioButtonClicked(View view) {
+        group = (RadioGroup) findViewById(R.id.radioGroup);
 
-            cv.put("name", (String) asd.getText());
+        int radioButtonID = group.getCheckedRadioButtonId();
 
-            OutputStream os = urlConnection.getOutputStream();
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(os,"UTF-8"));
-            bw.write(getQuery(cv));
-            bw.flush();
-            bw.close();
-            os.close();
-            int responseCode=urlConnection.getResponseCode();
-            String response = "";
-            if (responseCode == HttpsURLConnection.HTTP_OK) {
-                String line;
-                BufferedReader br=new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                while ((line=br.readLine()) != null) {
-                    response+=line;
+        radioButton = (RadioButton) findViewById(radioButtonID);
+
+        blood_type = radioButton.getText().toString();
+    }
+
+    public void sendPostRequest(View View) {
+        new PostClass(this).execute();
+    }
+
+    private class PostClass extends AsyncTask<String, Void, Void> {
+
+        private final Context context;
+        private ProgressDialog progress;
+
+        String nameValue =  name.getText().toString();
+        String dateValue = date_of_birth.getText().toString();
+        String phoneValue = phone_number.getText().toString();
+        String emailValue = email.getText().toString();
+        String cityValue = city.getText().toString();
+
+        public PostClass(Context c){
+            this.context = c;
+        }
+
+        protected void onPreExecute(){
+            progress= new ProgressDialog(this.context);
+            progress.setMessage("Loading");
+            progress.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+
+                final TextView outputView = (TextView) findViewById(R.id.showOutput);
+                URL url = new URL("http://192.168.88.167/GiveBlood/register_giver.php");
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+
+                cv.put("name", nameValue);
+                cv.put("date_of_birth", dateValue);
+                cv.put("phone_number", phoneValue);
+                cv.put("email", emailValue);
+                cv.put("blood_type", blood_type);
+                cv.put("city", cityValue);
+
+                connection.setRequestMethod("POST");
+                connection.setReadTimeout(10000);
+                connection.setConnectTimeout(15000);
+                connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
+                connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                DataOutputStream dStream = new DataOutputStream(connection.getOutputStream());
+                dStream.writeBytes(getQuery(cv));
+                dStream.flush();
+                dStream.close();
+                int responseCode = connection.getResponseCode();
+
+                final StringBuilder output = new StringBuilder("");
+                //output.append(System.getProperty("line.separator") + "Request Parameters " + cv);
+                //output.append(System.getProperty("line.separator")  + "Response Code " + responseCode);
+                //output.append(System.getProperty("line.separator")  + "Type " + "POST");
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                StringBuilder responseOutput = new StringBuilder();
+                System.out.println("output===============" + br);
+                while((line = br.readLine()) != null ) {
+                    responseOutput.append(line);
                 }
-            }
-            else {
-                response="";
+                br.close();
 
+                output.append(System.getProperty("line.separator") + "Response " + System.getProperty("line.separator") + System.getProperty("line.separator") + responseOutput.toString());
+
+                RegHelpMeActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+
+                        outputView.setText(output);
+                        progress.dismiss();
+
+                    }
+                });
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-            asd.setText(response);
-            urlConnection.connect();
-            urlConnection.disconnect();
+            return null;
         }
-        catch (MalformedURLException ex) {
-            Log.e("httptest",Log.getStackTraceString(ex));
-        }
-        catch (IOException ex) {
-            Log.e("httptest",Log.getStackTraceString(ex));
-        }*/
     }
+
     private String getQuery(ContentValues params) throws UnsupportedEncodingException
     {
         StringBuilder result = new StringBuilder();
@@ -100,5 +185,6 @@ public class RegHelpMeActivity extends AppCompatActivity {
 
         return result.toString();
     }
+
 
 }
